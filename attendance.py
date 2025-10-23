d5f6128a4f4df05f709848ed116c18887ba4681d
Daksha-Belawadi <belawadidaksha@gmailcom>
2025-10-23 16:27:17 +0530

    save to branch1

--- attendance.py
+++ attendance.py
@@ -1,4 +1,9 @@
 h=int(input("Held: "))
 a=int(input("Attended: "))
 p=(a/h)*100
-print("Attendance:",p,"%"
+print("Attendance:",p,"%")
+h=int(input("Held: "))
+a=int(input("Attended: "))
+p=(a/h)*100
+s="Eligible" if p>=75 else "Not Eligible"
+print(f"Attendance:{p}% Status:{s}")
